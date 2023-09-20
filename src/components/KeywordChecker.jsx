b4a36edd5d2465f6f4604139281e1152d4fc4745
vito2312 <93292401+vito2312@users.noreply.github.com>
2023-09-20 01:00:12 -0600

    Componentes funcionando

--- src/components/KeywordChecker.jsx
+++ src/components/KeywordChecker.jsx
@@ -1,19 +1,35 @@
 
-import { API_SERVER_URL } from "./Url";
-import React, {useState, useEffect} from 'react';
+import { API_SERVER_URL } from './Url';
+import React, { useState, useEffect } from 'react';
 
 const KeywordChecker = ({ text }) => {
-    const [isKeyword, setIsKeyword] = useState(false);
-  
-    useEffect(() => {
-      
-      fetch(`${API_SERVER_URL}/word?key=${text}`)
-        .then((response) => {console.log(response);return response.json();})
-        .then((data) => setIsKeyword(data.isKeyword))
-        .catch((error) => console.error('Error checking if text is a keyword:', error));
-    }, [text]);
-    
-    return null; 
-  };
-  
-  export default KeywordChecker;
+  const [isKeyword, setIsKeyword] = useState(false);
+
+  useEffect(() => {
+    const checkKeyword = async () => {
+      try {
+        const response = await fetch(`${API_SERVER_URL}/word?key=${text}`);
+        
+        if (!response.ok) {
+          throw new Error('La solicitud no tuvo éxito.');
+        }
+
+        const data = await response.json();
+        setIsKeyword(data.isKeyword);
+      } catch (error) {
+        console.error('Error checking if text is a keyword:', error);
+      }
+    };
+
+    if (text) {
+      checkKeyword();
+    } else {
+      // Si el texto está vacío, no es necesario hacer la solicitud
+      setIsKeyword(false);
+    }
+  }, [text]);
+
+  return null;
+};
+
+export default KeywordChecker;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,35 @@
-
 import { API_SERVER_URL } from "@/components/Url";
-
 import TextEditor from "@/components/TextEditor";
+import { useEffect, useState } from "react";
 
-const fetchGets = () =>{
+const fetchKeywords = async () => {
+  try {
+    const res = await fetch(`${API_SERVER_URL}/keywords`);
+    if (!res.ok) {
+      throw new Error('La solicitud no tuvo éxito.');
+    }
+    const data = await res.json();
+    return data.keywords || [];
+  } catch (error) {
+    console.error('Error fetching keywords:', error);
+    return [];
+  }
+};
 
-  return fetch(`${API_SERVER_URL}/keywords`)
-  .then(res=> res.json())
-  .catch(error => console.error('Error fetching keywords:', error))
-}
+export default function Home() {
+  const [keywordsList, setKeywordsList] = useState([]);
 
-export default async function Home() {
- const keywordsList = await fetchGets();
+  useEffect(() => {
+    fetchKeywords()
+      .then(keywords => setKeywordsList(keywords))
+      .catch(error => console.error('Error fetching keywords:', error));
+  }, []);
+
   return (
-    <>
-     <div className="App">
-          <TextEditor keywordsList={keywordsList} />
-      </div>
-    </>
-  )
+    <div className="Home">
+        <h1>One Flow Stream</h1>
+      <TextEditor keywordsList={keywordsList} />
+    </div>
+
+  );
 }
